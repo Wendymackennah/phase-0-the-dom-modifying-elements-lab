05853e2abf7984853b93340d8677341eef7ace17
wendymackennah <wendymackennah5@gmail.com>
2023-12-23 00:01:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// index.js
+
+// Remove the main#main node
+const mainNode = document.getElementById('main');
+if (mainNode) {
+  mainNode.remove();
+}
+
+// Create a new h1#victory node
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Weddy is the champion';
+
+// Append the new header to the document body or any other desired location
+document.body.appendChild(newHeader);
